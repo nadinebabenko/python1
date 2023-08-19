d58ede330b10f5f3e5f7a2905db7233a9aa7f34f
Nadine Babenko <119753982+nadinebabenko@users.noreply.github.com>
2023-08-19 15:35:30 +0300

    Add files via upload

---
+++ week20/day5/dayly.py
@@ -0,0 +1,23 @@
+#Create A Deck Of Cards Class.
+import random
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for suit in ['Hearts', 'Diamonds', 'Clubs', 'Spades']:
+            for value in ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']:
+                self.cards.append(Card(suit, value))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self):
+        if len(self.cards) == 0:
+            return None
+        else:
+            return self.cards.pop()
